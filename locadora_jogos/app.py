4470a727d2ef7cf0fe2baf1f0380e96c43bcaa8f
Daniel Latim <141602397+odaniellatim@users.noreply.github.com>
2025-09-19 18:27:57 -0300

    update 19/09/25

--- locadora_jogos/app.py
+++ locadora_jogos/app.py
@@ -1,7 +1,16 @@
-from Locadora import *
+#from locadora_jogos.odl_Locadora import *
+from Locadora import Locadora
+from base_dados import fabricantes_db, consoles_db, jogos_db
+
 
 if __name__ == "__main__":
-    locadora = Locadora()
+    
+    # Instancia da Classe Locadora
+    locadora = Locadora(
+        fabricantes_db,
+        consoles_db,
+        jogos_db)
     locadora.listar_jogos()
-    print("")
-    print("--- codigo finalizado ----")
+    #locadora.cadastrar_fabricante("game1","Game One", "Brasil", ["pc", "ps4"])
+    #locadora.cadastrar_fabricante("game2","Game two", "Africa", ["ps5", "xbox_series_x","ps4"])
+    locadora.listar_fabricantes("ps4")
--- locadora_jogos/base_dados.py
+++ locadora_jogos/base_dados.py
@@ -49,43 +49,50 @@ jogos_db = {
     "gta_v": {
         "nome": "Grand Theft Auto V",
         "categoria": "acao",
-        "preco_usd": 30,
+        "preco_usd": 39.90,
+        "alugado": False,
         "plataformas": ["ps4", "xbox_series_x"]
     },
     "god_of_war": {
         "nome": "God of War",
         "categoria": "acao",
-        "preco_usd": 60,
+        "preco_usd": 59.90,
+        "alugado": False,
         "plataformas": ["ps5"]
     },
     "halo_infinite": {
         "nome": "Halo Infinite",
         "categoria": "fps",
-        "preco_usd": 60,
+        "alugado": True,
+        "preco_usd": 59.98,
         "plataformas": ["xbox_series_x"]
     },
     "cyberpunk_2077": {
         "nome": "Cyberpunk 2077",
         "categoria": "rpg",
-        "preco_usd": 40,
+        "preco_usd": 39.59,
+        "alugado": False,
         "plataformas": ["ps5", "xbox_series_x", "pc"]
     },
     "the_witcher_3": {
         "nome": "The Witcher 3: Wild Hunt",
         "categoria": "rpg",
-        "preco_usd": 20,
+        "preco_usd": 22.59,
+        "alugado": True,
         "plataformas": ["ps4", "xbox_series_x", "pc", "nintendo_switch"]
     },
     "animal_crossing": {
         "nome": "Animal Crossing: New Horizons",
         "categoria": "simulacao",
-        "preco_usd": 60,
+        "preco_usd": 58.90,
+        "alugado": False,
         "plataformas": ["nintendo_switch"]
     },
     "fortnite": {
         "nome": "Fortnite",
         "categoria": "battle_royale",
-        "preco_usd": 0,
+        "preco_usd": 0.99,
+        "alugado": True,
         "plataformas": ["ps5", "xbox_series_x", "pc", "nintendo_switch"]
     }
 }
