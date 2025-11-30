2cc6c2058914574ac0c161409f0f271489b1b515
Daniel Latim <141602397+odaniellatim@users.noreply.github.com>
2025-11-30 00:12:38 -0300

    update gitignore

--- despesas.py
+++ despesas.py
@@ -9,18 +9,34 @@ agua = 140.00
 luz = 250
 claro = 300
 
-def math_soma(aluguel, cc_pf_nubank, cc_pj_nubank, cc_carrefour, agua, luz, claro):
+def math_soma(aluguel, cc_pf_nubank, cc_pj_nubank, cc_carrefour, agua, luz, claro) -> list :  
     # valor usado para calcular o valor de emergencia.
     EMERGENCY = 1.9
       
     # resultado da soma dos valores
-    total = aluguel + cc_pf_nubank + cc_pj_nubank + cc_carrefour + agua + luz + claro
+    total_aluguel = aluguel
+    total_cc = cc_pf_nubank + cc_pj_nubank + cc_carrefour
+    total_house = agua + luz + claro
+    
+    total = total_aluguel + total_cc + total_house
+    
     margem = ( total * EMERGENCY ) / 100
-    return [total, margem, total+margem]
+    return [
+        total_aluguel,
+        total_cc,
+        total_house,
+        total, 
+        margem, 
+        total + margem
+        ]
     
 resultado_math = math_soma(aluguel, cc_pf_nubank, cc_pj_nubank, cc_carrefour, agua, luz, claro)
 
-    
-print(f"Valor total: R$ {round(resultado_math[2], 2)}")
-print(f"Valor Emergencia: R$ {round(resultado_math[1], 2)}")
-print(f'Valor de cada tênis pintado: R$ {round(resultado_math[2] / qt_tenis_pintados, 2)}')
+print(f"Total Aluguel: R$ {round(resultado_math[0], 2)}")
+print(f"Total Cartão: R$ {round(resultado_math[1], 2)}")
+print(f"Total Casa: R$ {round(resultado_math[2], 2)}")
+print("-" * 30 )
+print(f"Valor total: R$ {round(resultado_math[3], 2)}")
+print(f"Valor Emergencia: R$ {round(resultado_math[4], 2)}")
+print(" " * 30)
+print(f'Valor de cada tênis pintado: R$ {round(resultado_math[3] / qt_tenis_pintados, 2)}')
